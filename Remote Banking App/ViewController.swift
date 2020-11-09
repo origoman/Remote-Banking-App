92b31f262555c64fcc0ffda665869261b917462f
Dmitriy Novikov <dmn@live.ru>
2020-11-09 12:50:36 +0300

    Initial Commit

---
+++ Remote Banking App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remote Banking App
+//
+//  Created by Dmitriy Novikov on 09.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
